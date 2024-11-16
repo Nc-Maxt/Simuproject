f7073cdf56ca632d083658fd5f83136362333e9b
Nc-Maxt <m.f.nunez.caceres@gmail.com>
2024-11-16 02:23:18 -0300

    Fix: read wrong in the previous fix and deleted the correct line, added again

--- UniformTrees/ust.py
+++ UniformTrees/ust.py
@@ -253,5 +253,7 @@ class Grafo:
             nocicled = erase_loops(cicled, dired)[0]
             # Se añade sin ciclo al grid
             self.append(nocicled)
+            # Se agrega el camino
+            paseos.append(nocicled)
         # Se retorna
         return paseos
