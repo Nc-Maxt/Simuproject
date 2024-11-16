b781c1fb3d86dc109cf0a74537d074fbdeba3970
Nc-Maxt <m.f.nunez.caceres@gmail.com>
2024-11-15 21:24:59 -0300

    Feat: Added new files to start the testing for the dualgraph and wilson

---
+++ Test/test2.py
@@ -0,0 +1,9 @@
+import numpy as np
+import sys
+import os.path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from UniformTrees.dust import rescalate, dualgraph
+
+
+
